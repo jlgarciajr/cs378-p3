ddfb369bf24e8e0cb3158544ed259c358293999e
Jose L Garcia <70235472+jlgarciajr@users.noreply.github.com>
2024-02-24 01:35:37 -0600

    Final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
+import { useState } from 'react';
 
 // import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
@@ -80,14 +81,84 @@ const menuItems = [
 
 
 function App() {
+
+  const [counts, setCounts] = useState(Array(menuItems.length).fill(0));
+
+  const reset = () => {
+    setCounts(Array(menuItems.length).fill(0));
+    setTotal(0);
+  };
+
+  const update = (index, newCount) => {
+    const newCounts = [...counts];
+    newCounts[index] = newCount;
+    setCounts(newCounts);
+    const subTotal = newCounts.reduce((acc, count, i) => acc + count * menuItems[i].price, 0);
+    setTotal(parseFloat(subTotal.toFixed(2)));
+  };
+
+  const [total, setTotal] = useState(0);
+
+  const showOrder = () => {
+    if (total > 0) {
+      let reciept = "Order Placed\n"
+
+      menuItems.forEach((item, index) => {
+        if (counts[index] > 0) {
+          reciept += `${item.title}: ${counts[index]}\n`;
+        }
+      });
+        
+      alert(reciept);
+
+    } else {
+      alert("You have no items in cart");
+    }
+  }
+
   return (
     <div>
-      <h1>Menu</h1>
+      <div className='row'>
+        <img className='logo' src={'images/ramen-tatsu-ya-logo-white.png'} alt='logo'></img>
+      </div>
+      <div className='row justify-content-center'>
+        <div className='col-8'>
+          <h3 className='subheader'>Japanese Soul Food</h3>
+        </div>
+      </div>
+      <div className='row justify-content-center'>
+        <div className='col-4'>
+          <img className='stylized-text' src={'images/RTY_2.0_Menubutton.png'} alt='stylized-text'></img>
+        </div>
+      </div>
+      <br></br>
       <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+        {menuItems.map((item, index) => (
+        <MenuItem
+          title={item.title}
+          description={item.description}
+          image={item.imageName}
+          price={item.price}
+          count={counts[index]}
+          setCount={(newCount) => update(index, newCount)}
+          total={total}
+          setTotal={setTotal}
+        /> 
+      ))}
+      </div>
+      <div class='row'>
+        <div class='col-6'>
+          <h3>Subtotal: ${total}</h3>
+        </div>
+        <div class='col-3'>
+          <button class="btn btn-outline-primary" onClick={showOrder}>Order</button>
+        </div>
+        <div class='col-3'>
+          <button class="btn btn-outline-primary" onClick={reset}>Clear All</button>
+        </div>
       </div>
     </div>
+    
   );
 }
 
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,16 +1,52 @@
 import React from 'react';
-
+import './MenuItem.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title }) => {
+
+
+
+const MenuItem = ({ title, description, image, price, count, setCount, total, setTotal }) => {
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+    
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className="row">
+            <div className='col-4'>
+                <img src={"images/" + image} className="card-img-top" alt={title}></img>
+            </div>
+            <div className='col-8'>
+                <div className='row'>
+                    <div className='col'>
+                        <h4>{title}</h4>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <p>{description}</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-7'>
+                        <p>${price}</p>
+                    </div>
+                    <div className='col- 5'>
+                        <button type="button" class="btn btn-outline-primary" onClick={increment}>+</button>
+                        <button type="button" class="btn btn-outline-primary">{count}</button>
+                        <button type="button" class="btn btn-outline-primary" onClick={decrement}>-</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
-
 export default MenuItem;
